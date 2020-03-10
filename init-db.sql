474e8f7419bbba009207093d8bfd0ee90ae0d46a
Shvedov Vladimir <shvedovvladimir27@yandex.ru>
2020-03-10 12:15:05 +0300

    fix init db

--- init-db.sql
+++ init-db.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE tandb_auth_db;
-CREATE USER postgres_app WITH PASSWORD 'postgres_password';
-GRANT ALL PRIVILEGES ON DATABASE "tandb_auth_db" to postgres_app;
+
+CREATE USER postgres_app_tandb_auth WITH PASSWORD 'postgres_password';
+
+GRANT ALL PRIVILEGES ON DATABASE "tandb_auth_db" to postgres_app_tandb_auth;
